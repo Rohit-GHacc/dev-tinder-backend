520720b3f0f101f34e62063ff1cdc0dfcee8403b
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-12-19 00:59:37 +0530

    Played with routing using regex mainly (), *, +, ?

--- src/app.js
+++ src/app.js
@@ -3,12 +3,33 @@ const app = express()
 
 
 // app.use(function(req,res){
-    //     res.send('response sent')
-    // })
+//     res.send('response sent')
+// })
+
+// THIS WILL MATCH TO ALL API HTTP METHODS LIKE GET POST PATCH
+app.use('/hello/hi',(req,res)=>{
+    res.send("Hi Rohit")
+})
 app.use('/hello',(req,res)=>{
     res.send('Hello Rohit')
 })
-    
+
+app.get('/user',(req,res)=>{
+    res.send('Got USER')
+})
+
+app.get(/ab*c/,(req,res)=>{
+    res.send('abXYZc')
+})
+
+app.get(/ab?c/,(req,res)=>{
+    res.send('ac or abc both will work')
+})
+
+app.get(/ab+cd/, (req,res)=>{
+    res.send('abbbbbcd should also work')
+})
+// USE BELOW ROUTE IN LAST EVERYTIME BECAUSE '/' ROUTE MATCHES EVERY ROUTE POSSIBLE
 app.use('/',(req,res)=>{
     res.send('Home page')
 })

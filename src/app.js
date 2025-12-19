0557d9bc71865ea0e8d9980cf787863ef1652cab
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-12-19 16:10:58 +0530

    Error handling and wildcard error handling too

--- src/app.js
+++ src/app.js
@@ -22,18 +22,31 @@ app.get('/user',
     },
     (req,res,next)=>{
         console.log("Third route handler")
-        res.send('Response 3')
+        // res.send('Response 3')
+        next()
     }]
 )
 
 // below code is never getting executed because response is sent above only
 app.get('/user',(req,res,next)=>{
-    res.send('Response 4')
+    // try{ 
+        throw new Error(' HAHAHAHAHA ')
+    //     res.send('Response 4')
+    // }
+    // catch(err){
+    //     res.status(500).send('Something went wrong. HAHAHA')
+    // }
+    // res.send('Response 4')
 })
 // USE BELOW ROUTE IN LAST EVERYTIME BECAUSE '/' ROUTE MATCHES EVERY ROUTE POSSIBLE
-// app.use('/',(req,res)=>{
-//     res.send('Home page')
-// })
+// WILDCARD ERROR HANDLING
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        // console.log(err)
+        res.status(500).send('Something went wrong. ')
+    }else
+    res.send('Home page')
+})
 app.listen(7777,()=>{
     console.log('Server running on port 7777 successfully.')
 })

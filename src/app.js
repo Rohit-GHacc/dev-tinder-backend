765316999fba0567b9ecef0aca710ffd443f6603
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-12-19 16:57:11 +0530

    Connected to DB using connection string and MONGOOSE after connecting to the cluster

--- src/app.js
+++ src/app.js
@@ -1,52 +1,11 @@
-const express = require('express')
-const app = express()
-const { userAuth, adminAuth } = require('./utils/middleware')
+const express = require("express");
+const db = require("./config/database");
+const app = express();
 
-app.get('/admin', adminAuth,(req,res)=>{
-    res.send('Admin Authorized.')
-})
-
-
-app.use('/user', userAuth)
-app.get('/user',
-    (req,res,next)=>{
-        console.log('First route handler')
-        // res.send('Response 1')
-        next();
-    },
-    // ARRAY ME BHI DAAL SAKTE HAIN INKO
-    [(req,res,next)=>{
-        console.log('Second route handler')
-        next()
-        // res.send('Response 2')
-    },
-    (req,res,next)=>{
-        console.log("Third route handler")
-        // res.send('Response 3')
-        next()
-    }]
-)
-
-// below code is never getting executed because response is sent above only
-app.get('/user',(req,res,next)=>{
-    // try{ 
-        throw new Error(' HAHAHAHAHA ')
-    //     res.send('Response 4')
-    // }
-    // catch(err){
-    //     res.status(500).send('Something went wrong. HAHAHA')
-    // }
-    // res.send('Response 4')
-})
-// USE BELOW ROUTE IN LAST EVERYTIME BECAUSE '/' ROUTE MATCHES EVERY ROUTE POSSIBLE
-// WILDCARD ERROR HANDLING
-app.use('/',(err,req,res,next)=>{
-    if(err){
-        // console.log(err)
-        res.status(500).send('Something went wrong. ')
-    }else
-    res.send('Home page')
-})
-app.listen(7777,()=>{
-    console.log('Server running on port 7777 successfully.')
-})
+// EXPORTED connectDB FROM database.js AND CHANGED THE VARIABLE NAME TO db  AND STILL WORKED
+db().then(() => {
+    console.log('Connected to database successfully')
+    app.listen(7777, () => {
+        console.log("Server running on port 7777 successfully.");
+    });
+});

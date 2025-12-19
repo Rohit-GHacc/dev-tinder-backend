befab583806e0364a5bb6d45250f2820f7fe4931
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-12-20 00:13:16 +0530

    1.) Added express.json middleware to convert request body to JSON object 2.) Change to dynamic data request 3.) Retrieved data from MongoDB using RestAPIs

--- src/app.js
+++ src/app.js
@@ -2,16 +2,10 @@ const express = require("express");
 const db = require("./config/database");
 const User = require('./models/user')
 const app = express();
-
+app.use(express.json())
 app.post('/createUser',async (req,res)=>{
-    const dummyUser = {
-        firstName : 'Virat',
-        lastName : 'Kohli',
-        email : 'virat@kohli.com',
-        password : 'rohit'
-    }
     try{
-        const user = new User(dummyUser)
+        const user = new User(req.body)
         await user.save();
         // console.log(User(dummyUser))
         res.send('user created successfully')
@@ -21,8 +15,30 @@ app.post('/createUser',async (req,res)=>{
     }
 
 })
+
+// DON'T FORGET TO USE ASYNC AWAIT
+app.get('/feed',async (req,res)=>{
+    try{
+        const users = await User.find()
+        res.send(users)
+    }
+    catch(err){
+        res.status(400).send("Something went wrong.")
+    }
+})
+
+app.get('/getUser', async (req,res)=>{
+    try{
+        const user = await User.find({email: req.body.email})
+        res.send(user)
+    }
+    catch(err){
+        res.status(400).send("Something went wrong.")
+    }
+})
 // EXPORTED connectDB FROM database.js AND CHANGED THE VARIABLE NAME TO db  AND STILL WORKED
-db().then(() => {
+db()
+.then(() => {
     console.log('Connected to database successfully')
     app.listen(7777, () => {
         console.log("Server running on port 7777 successfully.");

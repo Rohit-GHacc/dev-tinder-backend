7b2f8e6e33cf1f47f6ba5262d9632b9c59eb0603
Rohit-GHacc <rohitchugh142001@gmail.com>
2025-12-19 12:55:20 +0530

    Handling the response using multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,41 +1,30 @@
 const express = require('express')
 const app = express()
 
-
-// app.use(function(req,res){
-//     res.send('response sent')
-// })
-
-// THIS WILL MATCH TO ALL API HTTP METHODS LIKE GET POST PATCH
-app.use('/hello/hi',(req,res)=>{
-    res.send("Hi Rohit")
+app.get('/user',
+    (req,res,next)=>{
+        console.log('First route handler')
+        // res.send('Response 1')
+        next();
+    },
+    // ARRAY ME BHI DAAL SAKTE HAIN INKO
+    [(req,res,next)=>{
+        console.log('Second route handler')
+        next()
+        // res.send('Response 2')
+    },
+    (req,res,next)=>{
+        console.log("Third route handler")
+        next();
+    }]
+)
+app.get('/user',(req,res,next)=>{
+    res.send('Response 4')
 })
-app.use('/hello',(req,res)=>{
-    res.send('Hello Rohit')
-})
-
-app.get('/user',(req,res)=>{
-    res.send('Got USER')
-})
-
-app.post('/user',(req,res)=>{
-    console.log(req.query)
-    res.send("Request query ")
-})
-
-app.get('/user/:name/:password',(req,res)=>{
-    console.log(req.params)
-    res.send('PARAMS ADDED')
-})
-
-app.post('/user',(req,res)=>{
-    res.send('POST REQUEST SENT')
-})
-
 // USE BELOW ROUTE IN LAST EVERYTIME BECAUSE '/' ROUTE MATCHES EVERY ROUTE POSSIBLE
-app.use('/',(req,res)=>{
-    res.send('Home page')
-})
+// app.use('/',(req,res)=>{
+//     res.send('Home page')
+// })
 app.listen(7777,()=>{
     console.log('Server running on port 7777 successfully.')
 })

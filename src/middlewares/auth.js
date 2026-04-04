de527dfb8dd65e5b35acf868edc75bc8526afc84
Rohit-GHacc <rohitchugh142001@gmail.com>
2026-04-05 00:33:28 +0530

    changed cookie authentication to token authentication

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/user')
 const userAuth = async function(req,res,next){
     try{
-        const { token } = req.cookies
+        
+    const token = req.headers.authorization?.split(" ")[1];
         if(!token){
             return res.status(401).send("Please Login!")
         }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,15 +20,15 @@ authRouter.post("/createUser", async (req, res) => {
     await user.save();
     const token = await user.getJWT();
     // console.log(token)
-    res.cookie("token", token,{ 
-      expires: new Date(Date.now() + 900000), 
-      httpOnly: true, 
-      secure: true,        // must be true on HTTPS (Vercel/Render)
-      sameSite: "none", 
-    });
+    // res.cookie("token", token,{ 
+    //   expires: new Date(Date.now() + 900000), 
+    //   httpOnly: true, 
+    //   secure: true,        // must be true on HTTPS (Vercel/Render)
+    //   sameSite: "none", 
+    // });
 
     // ADD TOKEN TO COOKIE AND SEND THE RESPONSE BACK TO COOKIE
-    res.send(user);
+    res.send({user, token});
   } catch (err) {
     res.status(400).send("Error while creating the user" + err.message);
   }
@@ -57,13 +57,13 @@ authRouter.post("/login", async (req, res) => {
     // using mongooseSchema method : 
     const token = await user.getJWT();
     // console.log(token)
-    res.cookie("token", token,{ expires: new Date(Date.now() + 900000), 
-      httpOnly: true,
-      secure: true,        // must be true on HTTPS (Vercel/Render)
-      sameSite: "none", }
-    );
+    // res.cookie("token", token,{ expires: new Date(Date.now() + 900000), 
+    //   httpOnly: true,
+    //   secure: true,        // must be true on HTTPS (Vercel/Render)
+    //   sameSite: "none", }
+    // );
     // ADD TOKEN TO COOKIE AND SEND THE RESPONSE BACK TO COOKIE
-    res.send(user);
+    res.send({user ,token});
   } catch (err) {
     res.status(400).send("Login failed: " + err.message);
   }
